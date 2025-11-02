6aa44d5a654995f3c8ffba054bba936cad75130c
Torben Poguntke <t.poguntke@pitcon.de>
2025-11-02 16:03:27 +0100

    init

---
+++ methods/guest/src/main.rs
@@ -0,0 +1,45 @@
+use mithril_dwarf::*;
+use risc0_zkvm::guest::env;
+
+fn main() {
+    // read the genesis key
+    let start = env::cycle_count();
+    let mut genesis_key = vec![0u8; 32];
+    env::read_slice(&mut genesis_key);
+    let end = env::cycle_count();
+    eprintln!("read genesis key: {}", end - start);
+
+    // read the certificate input1
+    let start = env::cycle_count();
+    let len: usize = env::read();
+    let mut input_1 = vec![0u8; len];
+    env::read_slice(&mut input_1);
+    let end = env::cycle_count();
+    eprintln!("read certificate 1: {}", end - start);
+
+    let start = env::cycle_count();
+    let cert = certificate_from_bytes_fast(&input_1).expect("Failed to parse certificate 1");
+    let end = env::cycle_count();
+    eprintln!("parse certificate 1: {}", end - start);
+
+    // read the certificate input2
+    let start = env::cycle_count();
+    let len: usize = env::read();
+    let mut input_2 = vec![0u8; len];
+    env::read_slice(&mut input_2);
+    let prev_cert = certificate_from_bytes_fast(&input_2).expect("Failed to parse certificate 2");
+    let end = env::cycle_count();
+    eprintln!("read and parse certificate 2: {}", end - start);
+
+    // Business Logic
+    let start = env::cycle_count();
+    verify_standard_certificate(&cert, &prev_cert).expect(&format!(
+        "Failed to verify certificate 1, hash: '{:?}' against predecessor certificate 2, hash: '{:?}'",
+        cert.hash, prev_cert.hash
+    ));
+    let end = env::cycle_count();
+    eprintln!("verify_standard_certificate: {}", end - start);
+
+    // write public output to the journal
+    env::commit(&cert.hash);
+}
